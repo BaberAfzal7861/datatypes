9bf8d6eb8b4b6d10bfaaeb539fe1481aea80b2da
Baber Afzal <24BAfzal@heckgrammar.co.uk>
2024-09-27 14:13:50 +0100

    added some data types

--- data types/Program.cs
+++ data types/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // how to define a variable
+            // datatype identifier (optional assignment)
+            // eg:
+            // int = integer
+            int myInteger;
+            myInteger = 2147483647;
+            Console.WriteLine(myInteger);
+            // lowest 2^-31 and max is 2^31
+            //real/float:
+            //you have to put a captial F at the end of the number
+            float myFloat = 0.579F;
+            //higher precision of float is decimal but we have to put an m at the end
+            decimal myDecimal = 1.342946m;
+            // the best one to use is double we dont need a letter at the end
+            double myDouble = 1.23456;
+            //the next one on the list in boolean
+            // we shoud always write boolean with a capital B so Boolean
+            // in c sharp boolean is bool
+            bool myBool = true;
+            myBool = false;
+            // the next one is character which is singular character and its
+            // called in c sharp char and we reference them with a single speech mark
+            char myChar = 'p';
+            // next one is string and this is a collection of characters
+            // we identify a string with string and this is with double quotation marks
+            string myString = "this is a string";
+
         }
     }
 }
